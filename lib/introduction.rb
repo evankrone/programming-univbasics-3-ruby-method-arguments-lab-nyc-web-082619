56acdee4282821d1bd636cf8450e8de216e63c0a
Evan Krone <evankrone1@gmail.com>
2019-08-13 19:50:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,10 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-introduction("Steve")
+def introduction_with_language(name, language)
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
+
+end
 
 
 
